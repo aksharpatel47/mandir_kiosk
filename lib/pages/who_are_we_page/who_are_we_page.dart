d5bf525de9a6682e1523ac4a3ff380481118db73
Akshar Patel <akshar.patel.47@gmail.com>
2020-10-04 03:10:17 -0700

    feat: Scaffold who are we page
    See: Create Who Are We Page #2

--- lib/pages/who_are_we_page/who_are_we_page.dart
+++ lib/pages/who_are_we_page/who_are_we_page.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:mandir_kiosk/components/navigation/top_nav_bar.dart';
 
 class WhoAreWePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            TopNavBar(),
+            Expanded(
+              child: Center(
+                child: Text("Who are we?"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
